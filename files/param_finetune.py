76d593e0095cf3331e7c10cca8cd271340b2bd07
RicardodesaNunes <71496439+RicardodesaNunes@users.noreply.github.com>
2021-05-23 16:18:30 +0100

    Update param_finetune.py

--- files/param_finetune.py
+++ files/param_finetune.py
@@ -3,19 +3,23 @@ from charles.selection import rank
 from charles.mutation import swap_mutation
 from charles.crossover import cycle_co, single_point_co
 
-# x=Individual(fixed_vals={0:5,1:3,4:7,9:6,12:1,13:9,14:5,19:9,20:8,25:6,27:8,31:6,35:3,36:4,39:8,41:3,44:1,45:7,49:2,53:6,55:6,60:2,61:8,66:4,67:1,68:9,71:5,76:8,79:7,80:9})
-# print(x)
-# y=inversion_mutation(x)
-# print(y)
 
+# fixed values
 fixed = {0:5,1:3,4:7,9:6,12:1,13:9,14:5,19:9,20:8,25:6,27:8,31:6,35:3,36:4,39:8,41:3,44:1,45:7,49:2,53:6,55:6,60:2,61:8,66:4,67:1,68:9,71:5,76:8,79:7,80:9}
+# crossover methods
 xos = {'cycle_co':cycle_co, 'single_point_co':single_point_co}
+# crossover probabilities
 xo_ps = {f"0.{i}":i/10 for i in range(1,10)}
+# mutation probabilities
 mut_ps = {f"0.{i}":i/10 for i in range(1,10)}
 
+# for each crossover probability
 for xo_p in xo_ps:
+    # for each mutation probability
     for mut_p in mut_ps:
-        for xo in xos:  
+        # for each crossover method
+        for xo in xos:
+            # run 35 times each algorithm  
             for _ in range(35):    
                 
                 pop = Population(
