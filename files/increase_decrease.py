17a404a6f1ad1d9a41d69956151b3073915da12d
RicardodesaNunes <71496439+RicardodesaNunes@users.noreply.github.com>
2021-05-23 16:15:16 +0100

    Add files via upload

--- files/increase_decrease.py
+++ files/increase_decrease.py
@@ -1,8 +1,7 @@
-from charles.charles import Population, Individual
+from charles.charles import Population
 from charles.selection import rank
 from charles.mutation import swap_mutation
 from charles.crossover import single_point_co
-from random import randint, sample
 
 
 # x=Individual(fixed_vals={0:5,1:3,4:7,9:6,12:1,13:9,14:5,19:9,20:8,25:6,27:8,31:6,35:3,36:4,39:8,41:3,44:1,45:7,49:2,53:6,55:6,60:2,61:8,66:4,67:1,68:9,71:5,76:8,79:7,80:9})
@@ -19,7 +18,8 @@ for _ in range(35):
         size=120,
         optim="max",
         fixed_vals=fixed,
-        filename=f"inc_dec_file"
+        filename=f"\inc_dec_file",
+        folder=r"files\inc_dec"
     )
 
     pop.evolve(
--- files/param_finetune.py
+++ files/param_finetune.py
@@ -1,9 +1,7 @@
-from charles.charles import Population, Individual
+from charles.charles import Population
 from charles.selection import rank
 from charles.mutation import swap_mutation
 from charles.crossover import cycle_co, single_point_co
-from random import randint, sample
-
 
 # x=Individual(fixed_vals={0:5,1:3,4:7,9:6,12:1,13:9,14:5,19:9,20:8,25:6,27:8,31:6,35:3,36:4,39:8,41:3,44:1,45:7,49:2,53:6,55:6,60:2,61:8,66:4,67:1,68:9,71:5,76:8,79:7,80:9})
 # print(x)
@@ -24,8 +22,8 @@ for xo_p in xo_ps:
                     size=120,
                     optim="max",
                     fixed_vals=fixed,
-                    filename=f"xo_p_{xo_p.split('.')[1]}_mut_p_{mut_p.split('.')[1]}_{xo}",
-                    folder=r"files\finetune\"
+                    filename=f"\xo_p_{xo_p.split('.')[1]}_mut_p_{mut_p.split('.')[1]}_{xo}",
+                    folder=r"files\finetune"
                 )
 
                 pop.evolve(
