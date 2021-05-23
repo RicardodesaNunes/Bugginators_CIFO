2d816d321e7317cc6036fcf1123aabe82652cf1d
RicardodesaNunes <71496439+RicardodesaNunes@users.noreply.github.com>
2021-05-23 16:19:46 +0100

    Update sudoky.py

--- files/sudoky.py
+++ files/sudoky.py
@@ -3,20 +3,22 @@ from charles.selection import fps, tournament, rank
 from charles.mutation import swap_mutation, inversion_mutation, element_wise_mutation
 from charles.crossover import cycle_co, single_point_co, arithmetic_co
 
-
-# x=Individual(fixed_vals={0:5,1:3,4:7,9:6,12:1,13:9,14:5,19:9,20:8,25:6,27:8,31:6,35:3,36:4,39:8,41:3,44:1,45:7,49:2,53:6,55:6,60:2,61:8,66:4,67:1,68:9,71:5,76:8,79:7,80:9})
-# print(x)
-# y=inversion_mutation(x)
-# print(y)
-
+# fixed values
 fixed = {0:5,1:3,4:7,9:6,12:1,13:9,14:5,19:9,20:8,25:6,27:8,31:6,35:3,36:4,39:8,41:3,44:1,45:7,49:2,53:6,55:6,60:2,61:8,66:4,67:1,68:9,71:5,76:8,79:7,80:9}
+# selection methods
 selects = {'fps':fps, 'tournament':tournament, 'rank':rank}
+# crossover methods
 xos = {'cycle_co':cycle_co, 'single_point_co':single_point_co, 'arithmetic_co':arithmetic_co}
+# mutation methods
 muts = {'swap_mutation':swap_mutation, 'inversion_mutation':inversion_mutation, 'element_wise_mutation':element_wise_mutation}
 
+# for each selection method
 for sel in selects:
+    # for each crossover method
     for xo in xos:
-        for mut in muts:  
+        # for each mutation method
+        for mut in muts:
+            # run 35 times each algorithm
             for _ in range(35):    
                 
                 pop = Population(
